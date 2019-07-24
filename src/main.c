4d0986330bd4a674325d51b830a75e4a77c10d78
Andreas Kruhlmann <andreas@kruhlmann.com>
2019-07-25 01:31:17 +0200

    Added assert debugging

--- src/main.c
+++ src/main.c
@@ -6,26 +6,5 @@
 int main() {
     init();
 
-    short index = 0;
-    for (index = 0; index < BRD_TILE_CNT; index++) {
-        if (index % 10 == 0) {
-            printf("\n");
-        }
-        printf("%5d", sq_120_to_sq_64[index]);
-    }
-
-    printf("\n");
-    printf("\n");
-
-    for (index = 0; index < 64; index++) {
-        if (index % 8 == 0) {
-            printf("\n");
-        }
-        printf("%5d", sq_64_to_sq_120[index]);
-    }
-    
-    printf("\n");
-
-
     return 0;
 }
